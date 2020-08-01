46930b8073b5998b6cc367672e8fca3b18d2071d
yasirroni <muhammadyasirroni@gmail.com>
2020-08-01 20:38:32 +0700

    count n_out and file_as_str

--- mypower/count_nout.py
+++ mypower/count_nout.py
@@ -1,13 +1,22 @@
-# import oc2py
-# def count_nout(path):
-#     with open(path,):
-
-#     return
-
-# from oct2py import Oct2Py
-
-# def oc_matpower(path,oc=None):
-#     if oc == None:
-#         oc = Oct2Py()
-#     oc.addpath(oc.genpath(path))
-#     return oc
+def count_nout(path):
+    nout = 0
+    status = 'NOT FUNCTION'
+    with open(path, 'r') as f:
+        for l in f: 
+            if l[0] != 'f': #not function
+                if status == 'NOT FUNCTION':
+                    continue
+            l = l.translate(str.maketrans('', '', '[]()')).split()
+            try:
+                l.remove('function')
+            except:
+                pass
+            for s in l:
+                if s == '...':
+                    status = 'FUNCTION'
+                    continue
+                if s != '=':
+                    nout += 1
+                else:
+                    return nout
+    return nout
